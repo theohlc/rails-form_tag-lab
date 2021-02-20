e7c6e86f4fbd09818552b179767a29c37c053b60
theo <teddyhoover@gmail.com>
2021-02-20 13:59:28 -0600

    commit tree 5b4ac93f14bcba3064cb4a0a3243e79a397deb3f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -6,6 +8,21 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+    #binding.pry
+  end
+
+  def new
+    
+  end
+
+  def create
+    #binding.pry
+    @student = Student.new
+    @student.first_name= params["student"]["first_name"]
+    @student.last_name= params["student"]["last_name"]
+    #binding.pry
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
